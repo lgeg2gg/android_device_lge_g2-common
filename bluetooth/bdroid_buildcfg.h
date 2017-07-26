1a2957f39acda8fa861bc847878f57368d206c74
Nikola Majkić <majkforgit@gmail.com>
2017-07-26 18:06:58 +0200

    g2-common: bluetooth: Enable WBS BUGBASH-308
    Change-Id: I2ce1c0706f38559ab80370cf414234b85bf9d850

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -19,6 +19,7 @@
 
 #define BTM_DEF_LOCAL_NAME   "LG G2"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
+#define BTM_WBS_INCLUDED TRUE       /* Enable WBS */
 
 /* Defined if the kernel does not have support for CLOCK_BOOTTIME_ALARM */
 #define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE
